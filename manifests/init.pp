ade5df7a7d57cf1737e59af1b8c078e835d4126e
root <root@ip-172-31-22-97.us-west-1.compute.internal>
2019-11-06 15:13:20 -0500

    Refactored module for Hiera

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include apache
+class apache (
+  String $install_ensure,
+  String $install_name,
+) {
+   include apache::install
+
+}
---
+++ manifests/install.pp
@@ -0,0 +1,7 @@
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
+  }
+
+
+}
